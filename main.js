696f7b3e42c973ac752b32d85244410c571de94f
cuilei5205189 <allen5205189@gmail.com>
2018-07-29 20:09:57 +0800

    初步完成

---
+++ main.js
@@ -0,0 +1,23 @@
+$(p1).on('click', function(){
+    $(images).css({
+        transform: 'translate(0)'
+    })
+})
+
+$(p2).on('click', function () {
+    $(images).css({
+        transform: 'translate(-500px)'
+    })
+})
+
+$(p3).on('click', function () {
+    $(images).css({
+        transform: 'translate(-1000px)'
+    })
+})
+
+$(p4).on('click', function () {
+    $(images).css({
+        transform: 'translate(-1500px)'
+    })
+})
