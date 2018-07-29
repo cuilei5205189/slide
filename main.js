6591401d2e00b20fd6b30cd71734ecc32c25f9d7
cuilei5205189 <allen5205189@gmail.com>
2018-07-29 21:28:32 +0800

    改善版本

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-$(p1).on('click', function(){
-    $(images).css({
-        transform: 'translate(0)'
+var allButtons = $('#buttons > button')
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (e) {
+        var index = $(e.currentTarget).index()
+        var n = index * -500
+        $('#images').css({
+            transform: 'translate(' + n + 'px)'
+        })
     })
-})
+}
 
-$(p2).on('click', function () {
-    $(images).css({
-        transform: 'translate(-500px)'
-    })
-})
 
-$(p3).on('click', function () {
-    $(images).css({
-        transform: 'translate(-1000px)'
-    })
-})
 
-$(p4).on('click', function () {
-    $(images).css({
-        transform: 'translate(-1500px)'
-    })
+var n = 0
+var size = allButtons.length
+
+var timerId = setInterval(() => {
+    n += 1
+    allButtons.eq(n % size).trigger('click')
+        .addClass('red')
+        .siblings('.red').removeClass('red')
+}, 1000)
+
+$('.window').on('mouseenter', function (e) {
+    window.clearInterval(timerId)
+    console.log('mouseenter')
+
+})
+$('.window').on('mouseleave', function () {
+    console.log('mouseleave')
+    timerId = setInterval(() => {
+        n += 1
+        allButtons.eq(n % size).trigger('click')
+            .addClass('red')
+            .siblings('.red').removeClass('red')
+    }, 1000)
 })
