e9d97f279da971a17c87415c86fa9b2a640aacc2
cuilei5205189 <allen5205189@gmail.com>
2018-08-01 18:21:06 +0800

    done

--- seamless-slide2/main.js
+++ seamless-slide2/main.js
@@ -1,55 +1,125 @@
 let current = 0
-let $firstCopy = $('.images>img').eq(0).clone(true)
-let $lastCopy = $('.images>img').eq(2).clone(true)
-
-$('.images').append($firstCopy)
-$('.images').prepend($lastCopy)
-
-$('.images').css({
-    transform: 'translateX(-400px)'
+makeFakeSlides()
+bindEvents()
+$(next).on('click', function (e) {
+    goToSlide(current + 1)
 })
 
-$('button').eq(0).on('click', function (e) {
-    if (current == 2) {
-        console.log('最后到第一张')
-        $('.images').css({
-            transform: 'translateX(-1600px)'
-        }).one('transitionend', function () {
-            $('.images').hide().offset()
-            $('.images').css({
-                transform: 'translateX(-400px)'
-            }).show()
-        })
-    } else {
-        $('.images').css({
-            transform: 'translateX(-400px)'
-        })
-    }
-    current = 0
+$(pre).on('click', function (e) {
+    goToSlide(current - 1)
 })
 
-$('button').eq(1).on('click', function (e) {
-    $('.images').css({
-        transform: 'translateX(-800px)'
+let timer = setInterval(function () {
+    goToSlide(current + 1)
+}, 3000)
+
+$('.container').on('mouseenter', () => {
+    window.clearInterval(timer)
+    console.log('enter');
+}).on('mouseleave', () => {
+    console.log('leave')
+    timer = setInterval(() => goToSlide(current + 1), 2000)
+})
+
+
+function makeFakeSlides() {
+    let $firstCopy = $('.images>img').eq(0).clone(true)
+    let $lastCopy = $('.images>img').eq($('.buttons>button').length - 1).clone(true)
+    $('.images').append($firstCopy)
+    $('.images').prepend($lastCopy)
+}
+
+
+function bindEvents() {
+    $('.buttons').on('click', 'button', function (e) {
+        let $button = $(e.currentTarget)
+        let index = $button.index()
+        console.log($button.index())
+        goToSlide(index)
     })
-    current = 1
-})
+}
 
-$('button').eq(2).on('click', function (e) {
-    if (current == 0) {
-        console.log('最后到第一张')
+function goToSlide(index) {
+    if (index > $('.buttons>button').length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $('.buttons>button').length - 1
+    }
+    console.log(current, index);
+
+    if (current === $('.buttons>button').length - 1 && index === 0) {
+        //最后一张到第一张
         $('.images').css({
-            transform: 'translateX(0px)'
+            transform: `translateX(${-($('.buttons>button').length+1)*400}px)`
         }).one('transitionend', function () {
             $('.images').hide().offset()
             $('.images').css({
-                transform: 'translateX(-1200px)'
+                transform: `translateX(-400px)`
+            }).show()
+        })
+    } else if (current === 0 && index === $('.buttons>button').length - 1) {
+        //第一张到最后一张
+        $('.images').css({
+            transform: `translateX(0px)`
+        }).one('transitionend', function () {
+            $('.images').hide().offset()
+            $('.images').css({
+                transform: `translateX(${-(index+1)*400}px)`
             }).show()
         })
     } else {
         $('.images').css({
-            transform: 'translateX(-1200px)'
+            transform: `translateX(${-(index+1) * 400}px)`
         })
     }
-    current = 2
-})
+    current = index
+}
+
+// $('.images').css({
+//     transform: 'translateX(-400px)'
+// })
+
+// $('button').eq(0).on('click', function (e) {
+//     if (current == 2) {
+//         console.log('最后到第一张')
+// $('.images').css({
+//     transform: 'translateX(-1600px)'
+// }).one('transitionend', function () {
+//     $('.images').hide().offset()
+//     $('.images').css({
+//         transform: 'translateX(-400px)'
+//     }).show()
+// })
+//     } else {
+//         $('.images').css({
+//             transform: 'translateX(-400px)'
+//         })
+//     }
+//     current = 0
+// })
+
+// $('button').eq(1).on('click', function (e) {
+//     $('.images').css({
+//         transform: 'translateX(-800px)'
+//     })
+//     current = 1
+// })
+
+// $('button').eq(2).on('click', function (e) {
+//     if (current == 0) {
+//         console.log('最后到第一张')
+//         $('.images').css({
+//             transform: 'translateX(0px)'
+//         }).one('transitionend', function () {
+//             $('.images').hide().offset()
+//             $('.images').css({
+//                 transform: 'translateX(-1200px)'
+//             }).show()
+//         })
+//     } else {
+//         $('.images').css({
+//             transform: 'translateX(-1200px)'
+//         })
+//     }
+//     current = 2
+// })
